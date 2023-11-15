711c4abdd5bcf082373b2fb1a029c6baa591cb0e
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2023-11-15 10:19:28 -0800

    added testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,29 +1,44 @@
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 import '@testing-library/jest-dom'
+
+const allData = [
+  {
+    "name": "ginny joe",
+    "id": 3
+
+}
+];
+
  
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
- 
+ // test static heading
     const heading = screen.getByRole('heading', {
       name: /List of Names/i,
     })
  
     expect(heading).toBeInTheDocument()
-  })
-})        
-
-describe('Home', () => {
-    it('renders a link', () => {
+  })   
+  // test a dynamic element  
+  it('renders a link', () => {
       render(<Home />)
    
-      const heading = screen.getByRole('link', {
+      const myLink = screen.getByRole('link', {
         name: /Visit SRJC/i,
       })
    
-      expect(heading).toBeInTheDocument()
+      expect(myLink).toBeInTheDocument()
+    })        
+  // tests a dynamic element rendered by json
+  it('renders a link', () => {
+      render(<Home allData={allData}/>)
+   
+      const myLink2 = screen.getByRole('link', {
+        name: /ginny joe/i,
+      })
+   
+      expect(myLink2).toBeInTheDocument()
     })
-  })          
-
-  
+  })   
