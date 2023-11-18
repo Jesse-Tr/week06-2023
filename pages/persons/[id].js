61d5bf93c333d491a68c5dac13a2b62e7f85a823
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2023-11-17 20:25:38 -0800

    added firebase

--- pages/persons/[id].js
+++ pages/persons/[id].js
@@ -1,5 +1,5 @@
-import Layout from '../components/layout';
-import { getAllIds, getData } from '../lib/data';
+import Layout from '../../components/layout';
+import { getAllIds, getData } from '../../lib/data-firebase';
 
 // define a getStaticProps() function to have next.js retrieve data to use for the dynamic page
 // - this name is defined by next.js
@@ -16,7 +16,7 @@ export async function getStaticProps( { params } ) {
 // define a getStaticPaths() function to tell next.js all valid URLs: 1,2,3,4 
 // - this name is defined by next.js
 export async function getStaticPaths() {
-  const paths = getAllIds();
+  const paths = await getAllIds();
   return {
     paths,
     fallback: false
@@ -32,21 +32,32 @@ export default function Entry( { itemData } ) {
           <h4 className="card-title">{itemData.name}</h4>
           <h5 className="card-subtitle mb-2 text-body-secondary">{itemData.phone}</h5>
           <p className="card-text">{itemData.birthdate}</p>
-          <a href="#" className="card-link">{itemData.email}</a>
+          <a href="#" className="card-link">email: {itemData.email}</a>
+          <hr></hr>
+          <section>
           <h6>Nick Name</h6>
-          
-            {itemData.info && itemData.info.map(
-                ({id, nickname}) => (
-                  <dev key={id}>
-                    {nickname}
-                  </dev>
-                )
-              )
-            }
-          
+          <p>{itemData.nickname}</p>
+        </section>
+        
+        
 
         </div>
+
+
+    
       </article>
     </Layout>
   );
 }
+
+/*  original code for nickname from json file
+
+{itemData.persons && itemData.persons.map(
+  ({id, nickname}) => (
+    <dev key={id}>
+      {nickname}
+    </dev>
+  )
+)
+}
+*/
